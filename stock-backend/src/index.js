02cacc79dd1d31483f1ddde7f511e3dee976bcea
Lakshmi Priya <bl.en.u4cse22034@bl.students.amrita.edu>
2025-05-12 21:35:15 +0530

    update port

--- stock-backend/src/index.js
+++ stock-backend/src/index.js
@@ -6,7 +6,7 @@ import Router from "./routes.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
